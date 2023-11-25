40da527397f01b99fe27697699a004a65db87b58
nazmulhossen1 <nazmulhossen539@gmail.com>
2023-11-25 16:36:54 +0600

    collcent mongodb in the server side also texting

--- index.js
+++ index.js
@@ -7,10 +7,56 @@ const port = process.env.PORT || 5007;
 // Use the cors middleware
 app.use(cors());
 
+// smart-asset-managment
+// 2j7OAwG8JS12ApQ1
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://smart-asset-managment:2j7OAwG8JS12ApQ1@cluster0.idkvt6k.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+
+    app.get("/text", async (req, res) => {
+      res.send('thisis text1')
+    })
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 // Define a route
 app.get('/', (req, res) => {
   res.send('Smart asset manement server is running...!');
 });
+
+
+
 
 // Start the server
 app.listen(port, () => {
