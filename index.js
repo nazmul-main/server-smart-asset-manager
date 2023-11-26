d249140f1f2b168f8dc9854f602b3d80d8da33c5
nazmulhossen1 <nazmulhossen539@gmail.com>
2023-11-26 11:45:47 +0600

    create jwt in local storege when user sign in

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // app.js
 const express = require('express');
 const cors = require('cors'); // Import the cors middleware
+const jwt = require('jsonwebtoken');
 const app = express();
-const port = process.env.PORT || 5007;
-
-// Use the cors middleware
+require('dotenv').config();
+const port = process.env.PORT || 5001;
 app.use(cors());
+app.use(express.json())
 
 // smart-asset-managment
 // 2j7OAwG8JS12ApQ1
@@ -29,7 +30,39 @@ async function run() {
     // await client.connect();
 
 
-    app.get("/text", async (req, res) => {
+
+    // /*  jwt post api */
+    app.post('/api/v1/jwt', async (req, res) => {
+      const user = req.body;
+      console.log(req.body);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
+      })
+      res.send({ token });
+    })
+
+    // middleware 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access 1' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access 2' })
+        }
+        console.log(decoded);
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
+
+     
+
+    app.get('/api/v1/text', async (req, res) => {
       res.send('thisis text1')
     })
 
